35469779edeb29f9bc016a2b1b9b25aa04925f31
Suvamsh Shivaprasad <ssuvamsh@gmail.com>
2020-05-06 16:55:44 -0700

    changed some text

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,27 +61,24 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Software to manage <span className="text-color-primary">properties</span> by <span className="text-color-primary">property managers</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Manage properties like hotels from your desktop or mobile device. Track employee progress and property status.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
-                    </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
-                    </Button>
+                  <Button tag="a" color="primary" wideMobile href="https://www.suvamsh.com">
+                    Sign Up
+                  </Button>
                 </ButtonGroup>
               </div>
             </div>
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
             <a
-              data-video="https://player.vimeo.com/video/174002812"
+              data-video="https://www.youtube.com/embed/jdXjerxt6vQ"
               href="#0"
               aria-controls="video-modal"
               onClick={openModal}
@@ -98,7 +95,7 @@ const Hero = ({
             id="video-modal"
             show={videoModalActive}
             handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
+            video="https://www.youtube.com/embed/jdXjerxt6vQ"
             videoTag="iframe" />
         </div>
       </div>
